49516dd312b8962c43ba0bc8199b853eede29d91
Thige254 <thigetj@gmail.com>
2023-08-16 11:15:58 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,29 @@
+#!/usr/bin/python3
+
+def compute_difference(values):
+    max_val = max(values)
+    return max_val - sum(val for val in values if val != max_val)
+
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
 
     roman_numerals = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+        'C': 100, 'D': 500, 'M': 1000
     }
 
     total = 0
-    prev_value = 0
+    current_values = [0]
 
-    for symbol in roman_string:
-        current_value = roman_numerals[symbol]
-        
-        if prev_value and prev_value < current_value:
-            total += current_value - 2 * prev_value
-        else:
-            total += current_value
-        
-        prev_value = current_value
+    for char in roman_string:
+        if char in roman_numerals:
+            if roman_numerals[char] > max(current_values):
+                total += compute_difference(current_values)
+                current_values = [roman_numerals[char]]
+            else:
+                current_values.append(roman_numerals[char])
+
+    total += compute_difference(current_values)
 
     return total
