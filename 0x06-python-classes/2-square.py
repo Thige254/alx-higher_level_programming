4b606a95108789da9787d2fe5a8dfc9a7460551c
Thige254 <thigetj@gmail.com>
2023-08-30 08:48:16 +0000

    4-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
-    """Represent a square with validations.
-    
-    Attributes:
-        __size (int): The size of the square, validated to be an
-        integer and 
-                      greater than or equal to 0.
-    """
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square with validations.
+        """Initialize a new Square.
 
         Args:
-            size (int, optional): The size of the new square.
-            Defaults to 0.
-            
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is negative.
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a class to represent a Square."""
+
+class Square:
+    """Class that represents a geometric square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with a given size.
+
+        Args:
+            size (int): The edge length of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get or set the current edge length of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the size ensuring it's a valid integer and non-negative."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return (self.__size * self.__size)
