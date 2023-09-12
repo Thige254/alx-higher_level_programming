2243868887f155aaace00933b98c8cf34d9aab32
Thige254 <thigetj@gmail.com>
2023-09-12 20:57:33 +0000

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,39 +1,45 @@
 #!/usr/bin/python3
-"""Computes and prints metrics from standard input."""
+"""
+This module reads from standard input and computes metrics.
 
-def print_stats(size, status_codes):
-    """Print metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+the module prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
 
-    Args:
-        size (int): The accumulated read file size.
-        status_codes (dict): The accumulated count of status codes.
-    """
-    print("File size: {}".format(size))
-    for key in sorted(status_codes.keys()):
-        if status_codes[key]:
-            print("{}: {}".format(key, status_codes[key]))
+import sys
 
-if __name__ == "__main__":
-    import sys
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
+total_size = 0
+line_count = 0
 
-    line_count = 0
-    total_size = 0
-    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-                    '403': 0, '404': 0, '405': 0, '500': 0}
 
-    try:
-        for line in sys.stdin:
-            line_parts = line.split()
-            total_size += int(line_parts[-1])
-            if line_parts[-2] in status_codes:
-                status_codes[line_parts[-2]] += 1
+def print_stats():
+    """Prints statistics of status codes and file size."""
+    print("File size: {}".format(total_size))
+    for key, value in sorted(status_codes.items()):
+        if value > 0:
+            print("{}: {}".format(key, value))
 
-            line_count += 1
-            if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
 
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-        raise
+try:
+    for line in sys.stdin:
+        split_line = line.split()
+        size = split_line[-1]
+        status = split_line[-2]
+        total_size += int(size)
 
-    print_stats(total_size, status_codes)
+        if status in status_codes:
+            status_codes[status] += 1
+
+        line_count += 1
+
+        if line_count % 10 == 0:
+            print_stats()
+
+except KeyboardInterrupt:
+    pass
+finally:
+    print_stats()
