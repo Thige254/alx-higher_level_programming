6cbdd05a77c47b8534e4ce02916dffbf71b055ff
Thige254 <thigetj@gmail.com>
2023-09-12 20:26:32 +0000

    8-main_2.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a Class.
+
+    Returns:
+        A dictionary representation of the object.
+    """
+    return obj.__dict__
