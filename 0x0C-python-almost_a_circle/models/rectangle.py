0cb557929137c1f7f4ca424e61a37f74a322cfec
Thige254 <thigetj@gmail.com>
2023-09-19 06:50:29 +0000

    rectangle correction

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,124 +1,48 @@
 #!/usr/bin/python3
+"""Module for Square class that inherits from Rectangle."""
 
-"""Module for Rectangle class that inherits from Base."""
-from models.base import Base
+from .rectangle import Rectangle
 
 
-class Rectangle(Base):
-    """Rectangle class that inherits from Base."""
+class Square(Rectangle):
+    """Square class that inherits from Rectangle."""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle."""
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square."""
+        super().__init__(size, size, x, y, id)
 
-    @property
-    def width(self):
-        """Getter for width."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter for width."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y, self.width)
 
     @property
-    def height(self):
-        """Getter for height."""
-        return self.__height
+    def size(self):
+        """Getter for size."""
+        return self.width
 
-    @height.setter
-    def height(self, value):
-        """Setter for height."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Getter for x."""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Setter for x."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Getter for y."""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Setter for y."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """
-        Returns the area of the rectangle.
-        Formula for rectangle area: width * height
-        """
-        return self.__width * self.__height
-
-    def display(self):
-        """Displays the rectangle."""
-        # Print the top padding (value of y)
-        print("\n" * self.y, end="")
-
-        # For each line in the height
-        for _ in range(self.height):
-            # Print the left padding (value of x)
-            print(" " * self.x, end="")
-            # Print the rectangle width
-            print("#" * self.width)
-
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
 
     def update(self, *args, **kwargs):
-        """Update the Rectangle attributes."""
-
-        # List of attributes for the Rectangle
-        attributes = ["id", "width", "height", "x", "y"]
-
-        # Check if args exists and is not empty
-        if args and len(args) > 0:
+        """Update attributes of the Square instance."""
+        if args:
+            attributes = ["id", "size", "x", "y"]
             for i, value in enumerate(args):
                 if i < len(attributes):
                     setattr(self, attributes[i], value)
-        else:
+        elif kwargs:
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
 
-
     def to_dictionary(self):
-        """Returns the dictionary representation of a Rectangle."""
+        """Returns the dictionary representation of a Square."""
         return {
             'id': self.id,
-            'width': self.width,
-            'height': self.height,
+            'size': self.size,
             'x': self.x,
             'y': self.y
         }
-
-
-    def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y, self.width, self.height)
