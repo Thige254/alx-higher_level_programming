82ead030c2172c0fb628ab986b6faf68ab34f872
Thige254 <thigetj@gmail.com>
2023-09-19 07:26:10 +0000

    base.py tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -19,3 +19,37 @@ class TestBase(unittest.TestCase):
     def test_to_json_string_None(self):
         """ Test to_json_string with None """
         self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_to_json_string_None(self):
+        """ Test to_json_string with None """
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_to_json_string_empty_list(self):
+        """ Test to_json_string with an empty list """
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_to_json_string_list_of_dictionaries(self):
+        """ Test to_json_string with a list containing a dictionary """
+        self.assertEqual(Base.to_json_string([{'id': 12}]),
+                         '[{"id": 12}]')
+
+    def test_to_json_string_returns_string(self):
+        """ Test that to_json_string returns a string """
+        self.assertIsInstance(Base.to_json_string([{'id': 12}]), str)
+
+    def test_from_json_string_None(self):
+        """ Test from_json_string with None """
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_string_empty_string(self):
+        """ Test from_json_string with an empty string """
+        self.assertEqual(Base.from_json_string("[]"), [])
+
+    def test_from_json_string_string_of_list_of_dictionaries(self):
+        """ Test from_json_string with a string representing a list of dictionaries """
+        self.assertEqual(Base.from_json_string('[{"id": 89}]'),
+                         [{"id": 89}])
+
+    def test_from_json_string_returns_list(self):
+        """ Test that from_json_string returns a list """
+        self.assertIsInstance(Base.from_json_string('[{"id": 89}]'), list)
