1260d24512f140054cfa744de7d7bec7559ab28b
Thige254 <thigetj@gmail.com>
2023-10-17 20:27:04 +0300

    101 and 102 revised

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,10 +1,7 @@
--- Displays the average temperature by city,in descending order
-SELECT 
-    city, 
-    AVG(temperature) AS avg_temp 
-FROM 
-    temperatures
-GROUP BY 
-    city 
-ORDER BY 
-    avg_temp DESC;
+-- Script calculates the average temperature for each city,
+-- then sort the results by descending temperature
+SELECT city AS "city", 
+       AVG(value) AS "avg_temp"
+FROM temperatures
+GROUP BY city  -- average temps
+ORDER BY avg_temp DESC;  -- descending average temps
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,9 @@
--- Retrieve the top 3 cities with the highest average temperatures during the months of July and August
-SELECT city, AVG(value) AS average_temperature
+-- Pick the top 3 cities based on their average temperature in
+-- July and August, sorting by descending temperature
+SELECT city AS "city", 
+       AVG(value) AS "avg_temp"
 FROM temperatures
-WHERE month IN (7, 8)  -- Filtering for July and August
-GROUP BY city
-ORDER BY average_temperature DESC  -- Sorting in descending order by the average temperature
-LIMIT 3;  -- Limiting the results to the top 3
+WHERE month IN (7, 8)  -- Month of July & August
+GROUP BY city  -- Average by City
+ORDER BY avg_temp DESC  -- Order results by descending average temperature
+LIMIT 3;  -- Only top 3 cities
