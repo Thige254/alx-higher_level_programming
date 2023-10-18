d75b59350dd0ffb45332e417247a7c6e13fe6429
Thige254 <thigetj@gmail.com>
2023-10-18 17:22:52 +0300

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,11 +1,3 @@
 -- Script lists all privileges of the MySQL users 'user_0d_1' & 'user_0d_2' on the localhost server
-
--- Lists privileges for user_0d_1 if exists
-SELECT CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';') AS query
-FROM mysql.user
-WHERE user = 'user_0d_1' AND host = 'localhost';
-
--- Lists privileges for user_0d_2 if exists
-SELECT CONCAT('SHOW GRANTS FOR ''', user, '''@''', host, ''';') AS query
-FROM mysql.user
-WHERE user = 'user_0d_2' AND host = 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
