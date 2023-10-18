b2211f17e02c088009cd401329dc8c165adf295e
Thige254 <thigetj@gmail.com>
2023-10-18 16:34:23 +0300

    first file check correction

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,13 @@
--- Lists all privileges of the MySQL users 'user_0d_1' & 'user_0d_2' on the localhost server
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Script lists all privileges of the MySQL users 'user_0d_1' & 'user_0d_2' on the localhost server
+
+-- This down here lists privileges for user_0d_1
+DO @exists := EXISTS (SELECT 1 FROM mysql.user WHERE user='user_0d_1' AND host='localhost');
+IF @exists THEN
+    SHOW GRANTS FOR 'user_0d_1'@'localhost';
+END IF;
+
+-- This down here lists privileges for user_0d_2
+DO @exists := EXISTS (SELECT 1 FROM mysql.user WHERE user='user_0d_2' AND host='localhost');
+IF @exists THEN
+    SHOW GRANTS FOR 'user_0d_2'@'localhost';
+END IF;
