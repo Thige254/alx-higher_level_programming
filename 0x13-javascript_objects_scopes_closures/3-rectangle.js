8acc2c4816c2630e700c598e6ba9675d565d9ab7
Thige254 <thigetj@gmail.com>
2023-11-14 20:09:24 +0300

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,27 +1,25 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
+    constructor(w, h) {
       if (w > 0 && h > 0) {
         this.width = w;
         this.height = h;
       } else {
-        // If w or h is not a positive integer, create an empty object
         Object.assign(this, {});
       }
     }
-  
-    print () {
-      // Print the rectangle using the character X
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
+    print() {
+      if (this.width !== undefined && this.height !== undefined) {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
       }
     }
   }
   
   module.exports = Rectangle;
   
-  // Test the class
   const r1 = new Rectangle(2, 3);
   r1.print();
   
