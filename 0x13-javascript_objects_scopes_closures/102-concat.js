a4a7186bb2b491b27f3b5da7116cd23f82eea795
Thige254 <thigetj@gmail.com>
2023-11-14 21:39:52 +0300

    102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -13,3 +13,13 @@ const concatenatedContent = fArg + sArg;
 
 // Write the concatenated content to the destination file (process.argv[4])
 fs.writeFileSync(process.argv[4], concatenatedContent);
+#!/usr/bin/node
+// Use the fs (File System) module to perform file operations
+const fs = require('fs');
+
+// Read the content of the first source file (process.argv[2])
+const fArg = fs.readFileSync(process.argv[2]).toString();
+// Read the content of the second source file (process.argv[3])
+const sArg = fs.readFileSync(process.argv[3]).toString();
+
+fs.writeFileSync(process.argv[4], fArg + sArg);
