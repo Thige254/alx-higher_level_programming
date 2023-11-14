cc28649cc52639328818d427dd4649053c7785b8
Thige254 <thigetj@gmail.com>
2023-11-14 21:27:13 +0300

    102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,27 +1,15 @@
 #!/usr/bin/node
+// Use the fs (File System) module to perform file operations
 const fs = require('fs');
 
-// Check if the correct number of command-line arguments is provided
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
+// Read the content of the first source file (process.argv[2])
+const fArg = fs.readFileSync(process.argv[2]).toString();
 
-try {
-  // Read the content of the first source file
-  const fileAContent = fs.readFileSync(process.argv[2], 'utf8');
+// Read the content of the second source file (process.argv[3])
+const sArg = fs.readFileSync(process.argv[3]).toString();
 
-  // Read the content of the second source file
-  const fileBContent = fs.readFileSync(process.argv[3], 'utf8');
+// Concatenate the content of the two files
+const concatenatedContent = fArg + sArg;
 
-  // Concatenate the content of the two files
-  const concatenatedContent = fileAContent + fileBContent;
-
-  // Write the concatenated content to the destination file
-  fs.writeFileSync(process.argv[4], concatenatedContent);
-
-  console.log('Concatenation successful!');
-} catch (error) {
-  console.error('Error:', error.message);
-  process.exit(1);
-}
+// Write the concatenated content to the destination file (process.argv[4])
+fs.writeFileSync(process.argv[4], concatenatedContent);
