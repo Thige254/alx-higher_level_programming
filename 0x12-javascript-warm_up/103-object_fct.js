cfdb054e971068b046bf164480e03cdda6e74ef2
Thige254 <thigetj@gmail.com>
2023-11-15 18:35:45 +0300

    102 and 103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,20 @@
 #!/usr/bin/node
 
 const myObject = {
-    type: 'object',
-    value: 12
-  };
-  console.log(myObject);
-  myObject.incr = function () {
+  type: 'object',
+  value: 12,
+  incr: function () {
     this.value++;
-  };
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+  }
+};
+
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
+
+myObject.incr();
+console.log(myObject);
