58f7372f9b49e4d0c7264c81bda82c4814023509
Thige254 <thigetj@gmail.com>
2023-11-23 12:33:50 +0300

    city and states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+Script to fetch and print City objects from the database hbtn_0e_14_usa,
+displaying the city id, city name, and the corresponding state name.
 """
 import sys
-from model_state import Base, State
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Create a SQLAlchemy engine to connect to the MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # Bind the engine to the Base class (metadata)
+    Base.metadata.bind = engine
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and list all City objects with corresponding State names
+    for city, state in session.query(City, State).filter(City.state_id == State.id).order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
