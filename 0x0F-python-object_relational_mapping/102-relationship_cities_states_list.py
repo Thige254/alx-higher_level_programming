c82aeb6052f6f552f607695639607eb635590192
Thige254 <thigetj@gmail.com>
2023-11-23 12:21:59 +0300

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+Script to list all City objects from the database hbtn_0e_101_usa,
+displaying the city id, city name, and the corresponding state name.
 """
-from sqlalchemy import Column, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    # Create a SQLAlchemy engine to connect to the MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-class State(Base):
-    """
-    Class with id and name attributes of each state
-    """
-    __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
+    # Bind the engine to the Base class (metadata)
+    Base.metadata.bind = engine
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and list all City objects with corresponding State names
+    for city, state in session.query(City, State).filter(City.state_id == State.id).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, state.name))
