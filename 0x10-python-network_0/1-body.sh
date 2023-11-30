1fd6b85a5984e2b97c180a9914bbf3035f3dc4df
Thige254 <thigetj@gmail.com>
2023-11-30 14:11:40 +0300

    spel

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This Bash script sends a GET request
+# This Bash script sends a GET request to a given URL and displays the body of the response for a 200 status code.
 
-curl -sL "$1"
+curl -sLw "\n%{http_code}" "$1" | grep -w "200" | cut -d$'\n' -f1
