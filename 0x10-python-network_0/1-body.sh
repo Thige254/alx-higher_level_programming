923f056aa5338751818e2993c70d4622eba7d2b0
Thige254 <thigetj@gmail.com>
2023-12-05 17:21:54 +0000

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This Bash script sends a GET request to a given URL and displays the body of the response for a 200 status code.
-
-curl -sLw "\n%{http_code}" "$1" | grep -w "200" | cut -d$'\n' -f1
+# Bash script to send a GET request to a URL and display the body of the response
+url=$1; [ -z "$url" ] && exit 1; response=$(curl -sI "$url"); status_code=$(echo "$response" | grep -iE "^HTTP/[0-9.]+ ([0-9]+)" | awk '{print $2}'); [ "$status_code" = "200" ] && curl -s "$url" || echo "Request failed with status code: $status_code"
