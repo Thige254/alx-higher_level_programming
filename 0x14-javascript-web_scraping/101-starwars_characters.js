bbbdfa6c4924c6329906d60b581175d57f6e7544
Thige254 <thigetj@gmail.com>
2024-01-03 16:58:49 +0000

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,39 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-  const charactersUrls = movieData.characters;
-
-  const fetchAndPrintCharacter = (characterUrl) => {
-    request(characterUrl, (characterError, characterResponse, characterBody) => {
-      if (characterError) {
-        console.error(characterError);
-        return;
-      }
-
-      if (characterResponse.statusCode !== 200) {
-        console.error(`Failed to retrieve character data. Status code: ${characterResponse.statusCode}`);
-        return;
-      }
-
-      const character = JSON.parse(characterBody);
-      console.log(character.name);
-    });
-  };
-
-  charactersUrls.forEach(fetchAndPrintCharacter);
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
