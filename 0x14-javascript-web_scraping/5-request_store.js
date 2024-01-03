ba8cfd1a6a34855a9290a126c8b7eb6d29a6619f
Thige254 <thigetj@gmail.com>
2024-01-03 16:46:14 +0000

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,28 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-const fileStream = fs.createWriteStream(filePath);
-
-request(url)
-  .on('error', (error) => {
-    console.error(`Error requesting ${url}: ${error.message}`);
-  })
-  .on('response', (response) => {
-    if (response.statusCode !== 200) {
-      console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
-      fileStream.close(); // Close the file stream on error
-    }
-  })
-  .pipe(fileStream);
-
-fileStream.on('finish', () => {
-  console.log(`Body response written to ${filePath}`);
-});
-
-fileStream.on('error', (error) => {
-  console.error(`Error writing to ${filePath}: ${error.message}`);
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
