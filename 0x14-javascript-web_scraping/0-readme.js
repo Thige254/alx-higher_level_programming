76380cfcc9894694069e6ec68e3e4ce825629f55
Thige254 <thigetj@gmail.com>
2024-01-03 16:14:59 +0000

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
-#!/usr/bin/python3
-def fizzbuzz():
-    for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif number % 3 == 0:
-            print("Fizz", end=" ")
-        elif number % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(number, end=" ")
+#!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
